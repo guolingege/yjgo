652fb0991658c8787fc6c91d0f681686e7e51003
Zeroing-ZY <zeroing-zy@hotmail.com>
2020-02-21 14:57:58 +0800

    update app/controller/demo/table/table_controller.go.

--- app/controller/demo/table/table_controller.go
+++ app/controller/demo/table/table_controller.go
@@ -2,6 +2,7 @@ package table
 
 import (
 	"github.com/gogf/gf/net/ghttp"
+	"yj-app/app/model"
 	"yj-app/app/service/utils/response"
 )
 
@@ -76,3 +77,40 @@ func Recorder(r *ghttp.Request) {
 func Search(r *ghttp.Request) {
 	response.WriteTpl(r, "demo/table/search.html")
 }
+
+type us struct {
+	UserId      int64   `json:"userId"`
+	UserCode    string  `json:"userCode"`
+	UserName    string  `json:"userName"`
+	UserSex     string  `json:"userName"`
+	UserPhone   string  `json:"userPhone"`
+	UserEmail   string  `json:"userEmail"`
+	UserBalance float64 `json:"userBalance"`
+	Status      string  `json:"status"`
+	CreateTime  string  `json:"createTime"`
+}
+
+func List(r *ghttp.Request) {
+	var rows = make([]us, 0)
+	for i := 1; i <= 10; i++ {
+		var tmp us
+		tmp.UserId = int64(i)
+		tmp.UserName = "测试" + string(i)
+		tmp.Status = "0"
+		tmp.CreateTime = "2020-01-12 02:02:02"
+		tmp.UserBalance = 100
+		tmp.UserCode = "100000" + string(i)
+		tmp.UserSex = "0"
+		tmp.UserPhone = "15888888888"
+		tmp.UserEmail = "111@qq.com"
+		rows = append(rows, tmp)
+	}
+	r.Response.WriteJsonExit(model.TableDataInfo{
+		Code:  0,
+		Msg:   "操作成功",
+		Total: len(rows),
+		Rows:  rows,
+	})
+}
+
+
