6d7bded7bb81385424aa48b16dd79af68f5f3e41
Zeroing-ZY <zeroing_zy@hotmail.com>
2020-02-26 17:31:00 +0800

    fixed bug

--- app/service/system/permission/permission_service.go
+++ app/service/system/permission/permission_service.go
@@ -14,7 +14,7 @@ func GetPermiButton(u interface{}, permission, funcName, text, aclassName, iclas
 	result := HasPermi(u, permission)
 
 	htmlstr := ""
-	if result {
+	if result == "" {
 		htmlstr = `<a class="` + aclassName + `" onclick="` + funcName + `" hasPermission="` + permission + `">
                     <i class="` + iclassName + `"></i> ` + text + `
                 </a>`
@@ -24,15 +24,15 @@ func GetPermiButton(u interface{}, permission, funcName, text, aclassName, iclas
 }
 
 //根据用户id和权限字符串判断是否有此权限
-func HasPermi(u interface{}, permission string) bool {
+func HasPermi(u interface{}, permission string) string {
 	if u == nil {
-		return false
+		return "disabled"
 	}
 
 	uid := gconv.Int64(u)
 
 	if uid <= 0 {
-		return false
+		return "disabled"
 	}
 	//获取权限信息
 	var menus *[]menuModel.EntityExtend
@@ -45,10 +45,10 @@ func HasPermi(u interface{}, permission string) bool {
 	if menus != nil && len(*menus) > 0 {
 		for i := range *menus {
 			if strings.EqualFold((*menus)[i].Perms, permission) {
-				return true
+				return ""
 			}
 		}
 	}
 
-	return false
+	return "disabled"
 }
